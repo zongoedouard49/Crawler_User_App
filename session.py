241cd721f9a3886c88d6b5cd27cacfb6add72cdb
Zongo Edouard <edouardzongo636399@gmail.com>
2026-09-22 16:22:33 +0000

    ssl

--- session.py
+++ session.py
@@ -12,7 +12,7 @@ DATABASE_URL = os.getenv("COCKROACH_URL")
 print(DATABASE_URL)
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"sslmode": "verify-full"},)
+    connect_args={"sslmode": "disable"},)
     # pool_pre_ping=True,
     # pool_size=10,
     # max_overflow=5,
